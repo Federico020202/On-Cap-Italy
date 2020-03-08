f737179c632ea3e9979e803398e83d00f37c240e
Federico <felixsanti2002@gmail.com>
2020-03-08 22:11:06 +0100

    dd
    d

---
+++ contactform.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST['submit'])) {
+
+    $Firstname = $_POST['Firstname'];
+    $Secondname = $_POST['econdname'];
+    $Email = $_POST['Email'];
+    $Subjectmessage = $_POST['Subjectmessage'];
+    $Message = $_POST['Message'];
+
+    $mailTo = "OnCapItaly@post.com";
+    $headers = "From: ".$Email;
+    $txt = "Hai ricevuto una mail da ".$Firstname".\n\n"$message;
+
+
+
+    mail($mailTo, $Subjectmessage, $txt, $headers);
+    header("Location: index.php?mailsend");
+
+
+}
